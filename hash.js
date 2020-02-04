f328b96b6d6c8b3b72fdef7102f53544c0d6d313
lydiali9 <648823000@qq.com>
2020-02-04 12:39:36 +0800

    [feature]: add some new function, for example: list stack queen singleLinkedList doubleLinkedList hash

---
+++ hash.js
@@ -0,0 +1,23 @@
+function HashTable() {
+	this.table = [];
+	this.simpleHash = simpleHash;
+	this.put = put;
+	this.get = get;
+	this.showDistor = showDistor;
+}
+
+function simpleHash(data) {
+
+}
+
+function put() {
+
+}
+
+function get(key) {
+
+}
+
+function showDistor() {
+
+}
